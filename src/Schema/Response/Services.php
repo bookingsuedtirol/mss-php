605a1ca4de70101726038e4839feeb058f03d52b
David Mair Spiess <david.spiess@hgv.it>
2016-04-07 09:39:18 +0200

    Make services inline

--- src/Schema/Response/Services.php
+++ src/Schema/Response/Services.php
@@ -2,11 +2,13 @@
 
 namespace MssPhp\Schema\Response;
 
+use JMS\Serializer\Annotation\Inline;
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\XmlList;
 
 class Services {
     /**
+     * @Inline;
      * @Type("array<integer>")
      * @XmlList(inline = true, entry = "service")
      */
