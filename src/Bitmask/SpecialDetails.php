bf77c229d1e8b5853345245e3a5c61ffc6359b48
David Mair Spiess <david.spiess@hgv.it>
2016-04-07 08:56:56 +0200

    Add special details bitmask

---
+++ src/Bitmask/SpecialDetails.php
@@ -0,0 +1,13 @@
+<?php
+namespace MssPhp\Bitmask;
+
+class SpecialDetails
+{
+    const BASIC_INFO = 1;
+    const TITLE = 2;
+    const DESCRIPTIONS = 4;
+    const SEASONS = 8;
+    const IMAGES = 16;
+    const THEMES = 32;
+    const INCLUDED_SERVICES = 64;
+}
