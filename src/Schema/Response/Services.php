47777d2d0d78cfd1532a671c0479b485b5998fa3
David Mair Spiess <david.spiess@hgv.it>
2016-04-07 09:37:53 +0200

    Fix services

--- src/Schema/Response/Services.php
+++ src/Schema/Response/Services.php
@@ -2,14 +2,12 @@
 
 namespace MssPhp\Schema\Response;
 
-use JMS\Serializer\Annotation\Inline;
 use JMS\Serializer\Annotation\Type;
 use JMS\Serializer\Annotation\XmlList;
 
 class Services {
     /**
-     * @Inline;
-     * @Type("array<MssPhp\Schema\Response\Service>")
+     * @Type("array<integer>")
      * @XmlList(inline = true, entry = "service")
      */
     public $service;
