9b3fa190410edf88f84d979585f49385d273abd5
David Mair Spiess <david.spiess@hgv.it>
2016-04-07 08:13:18 +0200

    Add bitmask for room details

---
+++ src/Bitmask/RoomDetails.php
@@ -0,0 +1,14 @@
+<?php
+namespace MssPhp\Bitmask;
+
+class RoomDetails
+{
+    const BASIC_INFO = 4;
+    const TITLE = 8;
+    const ROOM_IMAGES = 32;
+    const ROOM_FACILITIES_FILTER = 64;
+    const ROOM_DESCRIPTION = 256;
+    const ROOM_FACILITIES_DETAILS = 4096;
+    const ROOM_FEATURES = 32768;
+    const ROOM_NUMBERS = 65536;
+}
