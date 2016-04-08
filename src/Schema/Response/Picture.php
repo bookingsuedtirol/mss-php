9721eab8387c871158865755c8e99160e24cdda8
David Mair Spiess <david.spiess@hgv.it>
2016-04-08 08:50:35 +0200

    Replace easymailing domain with booking southtyrol

--- src/Schema/Response/Picture.php
+++ src/Schema/Response/Picture.php
@@ -2,13 +2,25 @@
 
 namespace MssPhp\Schema\Response;
 
+use JMS\Serializer\Annotation\AccessType;
 use JMS\Serializer\Annotation\Type;
 
 class Picture {
     /**
+     * @AccessType("public_method")
      * @Type("string")
      */
     public $url;
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = str_replace('http://www.easymailing.eu', 'https://www.bookingsuedtirol.com', $url);
+    }
 
     /**
      * @Type("integer")
