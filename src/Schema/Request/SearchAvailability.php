b1e593a07dd82bffb0b4b4a3310fbb615df9f61b
David Mair Spiess <david.spiess@hgv.it>
2017-11-27 09:35:03 +0100

    Add new filter possibilities

--- src/Schema/Request/SearchAvailability.php
+++ src/Schema/Request/SearchAvailability.php
@@ -3,6 +3,7 @@
 namespace MssPhp\Schema\Request;
 
 use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlList;
 
 class SearchAvailability {
     /**
@@ -14,4 +15,16 @@ class SearchAvailability {
      * @Type("DateTime<'Y-m-d'>")
      */
     public $date_to;
+
+    /**
+    * @Type("array<integer>")
+    * @XmlList(inline = true, entry="offer_id")
+     */
+    public $offer_id;
+    
+    /**
+    * @Type("array<integer>")
+    * @XmlList(inline = true, entry="room_id")
+     */
+    public $room_id;
 }
