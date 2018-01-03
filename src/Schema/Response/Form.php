f16ae67c18bf8b9d4319b37388231d01f4ae8249
David Mair Spiess <david.spiess@hgv.it>
2018-01-03 08:54:24 +0100

    Add new iframe settings

--- src/Schema/Response/Form.php
+++ src/Schema/Response/Form.php
@@ -3,12 +3,24 @@
 namespace MssPhp\Schema\Response;
 
 use JMS\Serializer\Annotation\Type;
+use JMS\Serializer\Annotation\XmlList;
 
 class Form {
     /**
      * @Type("string")
      */
     public $form_url;
+
+    /**
+     * @Type("integer")
+     */
+    public $form_iframe;
+
+    /**
+     * @Type("array<MssPhp\Schema\Response\FormMethods>")
+     * @XmlList(inline = true, entry = "form_methods")
+     */
+    public $form_methods;
 
     /**
      * @Type("MssPhp\Schema\Response\FormFields")
