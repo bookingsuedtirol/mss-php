f4323489ddb552f9f39553f53d590aa97f738f6e
TomOne <TomOne@users.noreply.github.com>
2021-05-13 08:20:45 +0200

    Add getHotelList example

---
+++ examples/getHotelList.php
@@ -0,0 +1,45 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+use Crutches\Bitmask;
+use MssPhp\Client;
+use MssPhp\Bitmask\HotelDetails;
+
+$client = new Client();
+
+$res = $client->request(function ($req) {
+    $req->header->method = "getHotelList";
+    $req->request->search->id[] = "9002";
+    $req->request->options->hotel_details = (new Bitmask(
+        HotelDetails::BASIC_INFO |
+            HotelDetails::THEMES |
+            HotelDetails::HOTEL_FACILITIES |
+            HotelDetails::SHORT_DESCRIPTION |
+            HotelDetails::FULL_DESCRIPTION |
+            HotelDetails::GEOGRAPHIC_INFORMATION |
+            HotelDetails::COORDINATES |
+            HotelDetails::ADDRESS |
+            HotelDetails::CONTACTS |
+            HotelDetails::PAYMENT_OPTIONS_FOR_ONLINE_BOOKING |
+            HotelDetails::PAYMENT_OPTIONS_AT_HOTEL |
+            HotelDetails::LOGO |
+            HotelDetails::HEADER_IMAGES |
+            HotelDetails::GALLERY |
+            HotelDetails::HOTEL_MATCHING |
+            HotelDetails::GEOGRAPHICAL_INFORMATION_AS_TEXT |
+            HotelDetails::HOTEL_NAVIGATOR_DATA |
+            HotelDetails::DETAILED_HOTEL_FACILITIES |
+            HotelDetails::SALES_POINT |
+            HotelDetails::LTS_SPECIFIC_PARAMETERS |
+            HotelDetails::CHECK_IN_OUT |
+            HotelDetails::SOURCE_DATA |
+            HotelDetails::BOARD_DATA |
+            HotelDetails::COUPON_SERVICE_DATA
+    ))->getBitmask();
+});
+
+echo json_encode($res, JSON_PRETTY_PRINT);
